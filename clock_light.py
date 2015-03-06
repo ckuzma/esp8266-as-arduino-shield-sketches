100228fb780e84a6c13a46b61eb0e4c18b6f6691
Chris Kuzma <chris.kuzma@gmail.com>
2015-03-05 19:44:45 -0500

    Changed on/off time selection to runtime parameters.

--- clock_light.py
+++ clock_light.py
@@ -9,21 +9,28 @@
 # -Change the time zone to the appropriate one for your location
 # -Upload "light_server.ino" to the Arduino and attach the ESP8266 as described
 # -Attach the relay / LED you want to toggle to pin 2 of the Arduino
-# -Run this script, setting on and off times as prompted (must follow syntax)
-#
-
+# -Modify IP address to the correct one
+# -Run this script
 
 from datetime import datetime
 from dateutil import tz
 import time
 import os
+import sys
 
-alarmTime = raw_input('Please enter in an alarm time (e.g. 08:00:00): ')
-lightOffTime = raw_input('Please enter in a light-off time (e.g. 08:30:00): ')
+if len(sys.argv) != 3:
+    print 'ERROR: Not enough parameters given.'
+    print '\nUsage:\t\tpython ' + sys.argv[0] + ' on off'
+    print 'Example:\tpython ' + sys.argv[0] + ' 08:00:00 08:15:00'
+    print '\n\tTime format (24h): hh:mm:ss\n\n'
+    exit()
+
+alarmTime = sys.argv[1]
+lightOffTime = sys.argv[2]
 lightStatus = 'Off'
 
 from_zone = tz.gettz('UTC')
-to_zone = tz.gettz('America/Indiana/Indianapolis') # Edit this to your own timezone
+to_zone = tz.gettz('America/Indiana/Indianapolis')
 
 x = 0
 while x < 1:
@@ -34,10 +41,10 @@ while x < 1:
     utc = utc[1].split('.')
     utc = utc[0]
     if utc == alarmTime:
-        os.system('curl -m 1 http://LOCAL-IP/?command=10E') # This turns my light on
+        os.system('curl -m 10 http://192.168.1.67/?command=0E')
         lightStatus = 'On'
     if utc == lightOffTime:
-        os.system('curl -m 1 http://LOCAL-IP/?command=11E') # This turns my light off
+        os.system('curl -m 10 http://192.168.1.67/?command=1E')
         lightStatus = 'Off'
     print 'Current Time\t' + utc
     print 'Alarm Time\t' + alarmTime
