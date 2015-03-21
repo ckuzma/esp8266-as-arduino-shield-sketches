7d83343f0792375b80086147009534794189437e
Chris Kuzma <chris.kuzma@gmail.com>
2015-03-20 21:54:02 -0400

    Enabled transmission of HTTP JSON blurb.

--- light_server.ino
+++ light_server.ino
@@ -14,6 +14,10 @@ Implemented improvements by Dir (http://www.esp8266.com/memberlist.php?mode=view
 and modified the setup routine to allow for a slower WiFi handshake when joining an AP. Should
 result in a much less buggy setup.
 
+Mar 20, 2015 @ 9:52p:
+Activated the JSON return blurb for an incoming HTTP request. Probably the last update this
+will ever see.
+
 ------
 
 Wiring Setup (Arduino UNO):
@@ -36,12 +40,11 @@ How To Use:
   the current HIGH/LOW status of all 9 controllable pins
 */
 
-
 #include <SoftwareSerial.h>
 #define BUFFER_SIZE 512
 #define GET_SIZE 64
 #define dbg Serial  // USB local debug
-SoftwareSerial esp(11,12);
+SoftwareSerial esp(11, 12);
 String ssid = "";
 String pass = "";
 String serverPort = "80";
@@ -50,34 +53,29 @@ char buffer[BUFFER_SIZE]; // Don't touch
 char get_s[GET_SIZE];
 char OKrn[] = "OK\r\n"; // Don't touch
 
-String currentCommand = "000000000";
+String currentCommand = "0000";
 
-
-byte waitForEsp(int timeout, char* term=OKrn) {
-  unsigned long t=millis();
-  bool found=false;
-  int i=0;
-  int len=strlen(term);
-  while(millis()<t+timeout) {
-    if(esp.available()) {
-      buffer[i++]=esp.read();
-      if(i>=len) {
-        if(strncmp(buffer+i-len, term, len)==0) {
-          found=true;
+byte waitForEsp(int timeout, char* term = OKrn) {
+  unsigned long t = millis();
+  bool found = false;
+  int i = 0;
+  int len = strlen(term);
+  while (millis() < t + timeout) {
+    if (esp.available()) {
+      buffer[i++] = esp.read();
+      if (i >= len) {
+        if (strncmp(buffer + i - len, term, len) == 0) {
+          found = true;
           break;
         }
       }
     }
   }
-  buffer[i]=0;
+  buffer[i] = 0;
   dbg.print(buffer);
   return found;
 }
 
-
-// ##################
-// ## Setup & Loop ##
-// ##################
 void setup() {
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -87,10 +85,10 @@ void setup() {
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
-  pinMode(10,OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(13, OUTPUT); // For debugging purposes w/o USB
   // Set baud rates
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
   esp.begin(9600);
   dbg.begin(9600);
   dbg.println("DEBUG: Running Setup");
@@ -101,94 +99,90 @@ void setup() {
   waitForEsp(2000);
   esp.println("AT+CWMODE=1"); // Set to client mode
   waitForEsp(2000);
-  esp.println("AT+CWJAP=\""+ssid+"\",\""+pass+"\""); // Join AP
+  esp.println("AT+CWJAP=\"" + ssid + "\",\"" + pass + "\""); // Join AP
+  delay(8000); // Sometimes joining is really slow...
   waitForEsp(6000);
   esp.println("AT+CIPMUX=1"); // Allow multiple connections
   waitForEsp(2000);
-  esp.println("AT+CIPSERVER=1,"+serverPort); // Start server on port
+  esp.println("AT+CIPSERVER=1," + serverPort); // Start server on port
   waitForEsp(2000);
   esp.println("AT+CIFSR");
   waitForEsp(1000);
   dbg.println("DEBUG: Setup complete\n\n");
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
 }
 void loop() {
   updateLights(currentCommand);
   // BEGIN - Borrowed Code
   int ch_id, packet_len, lssid = 0, lpass = 0;
-  char *pb;  
-  if(readTillEnd()) {
-    if(strncmp(buffer, "+IPD,", 5)==0) {
-      sscanf(buffer+5, "%d,%d", &ch_id, &packet_len);
+  char *pb;
+  if (readTillEnd()) {
+    if (strncmp(buffer, "+IPD,", 5) == 0) {
+      sscanf(buffer + 5, "%d,%d", &ch_id, &packet_len);
       if (packet_len > 0) {
-        pb = buffer+5;
-        while(*pb!=':') pb++;
+        pb = buffer + 5;
+        while (*pb != ':') pb++;
         pb++;
         incomingRequest(pb, ch_id);
       }
     }
   }
-  // END - Borrowed Code
 }
-// ##################
-// ## Setup & Loop ##
-// ##################
 
-
-void incomingRequest(char *pb, int ch_id){
+void incomingRequest(char *pb, int ch_id) {
   char *pget;
   if (strncmp(pb, "GET /favicon.ico", 16) == 0) {
-    pb = pb+6;
+    pb = pb + 6;
     waitForEsp(1000);
   }
   if (strncmp(pb, "GET /?", 6) == 0) {
-    pb = pb+6;
+    pb = pb + 6;
     pget = get_s;
-    while(*pb!=' '){
+    while (*pb != ' ') {
       *pget = *pb;
-      pb++; 
+      pb++;
       pget++;
     }
     extractCommand();
     waitForEsp(1000);
     dbg.println("-> serve homepage (Would've!)\n");
-    dbg.println("Command received: "+currentCommand);
-    serveReply(ch_id,currentCommand);
+    dbg.println("Command received: " + currentCommand);
+    serveReply(ch_id, currentCommand);
   }
 }
 
-void serveReply(int ch_id,String received){
-  String reply = "{current_command:"+received+"}";
-  esp.println("AT+CIPSEND="+String(ch_id)+","+String(reply.length())); // change 18 to reply length
-  waitForEsp(2000,">");
+void serveReply(int ch_id, String received) {
+  String reply = "{\"current_command\":" + received + "}";
+  esp.println("AT+CIPSEND=" + String(ch_id) + "," + String(reply.length())); // change 18 to reply length
+  waitForEsp(2000, ">");
   dbg.println("Sending back a response");
   esp.print(reply);
   waitForEsp(2000);
   dbg.println("Closing connection");
-  esp.println("AT+CIPCLOSE="+String(ch_id));
+  esp.println("AT+CIPCLOSE=" + String(ch_id));
   waitForEsp(2000);
 }
 
-void extractCommand(){
-  String url_command ="";
+void extractCommand() {
+  String url_command = "";
   char *pget;
   pget = get_s;
-  pget = pget+8; //len("?command")+1
-  while(*pget!='E'){ // Terminating thing on end of string (ex. 010011E)
+  pget = pget + 8; //len("?command")+1
+  while (*pget != 'E') { // Terminating thing on end of string (ex. 010011E)
     url_command += *pget;
-    pget++; 
+    pget++;
   }
   waitForEsp(2000);
   currentCommand = url_command;
 }
 bool readTillEnd() {
-  static int i=0;
-  if(esp.available()) {
-    buffer[i++]=esp.read();
-    if(i==BUFFER_SIZE)  i=0;
-    if(i>1 && buffer[i-2]==13 && buffer[i-1]==10) {
-      buffer[i]=0;
-      i=0;
+  static int i = 0;
+  if (esp.available()) {
+    buffer[i++] = esp.read();
+    if (i == BUFFER_SIZE)  i = 0;
+    if (i > 1 && buffer[i - 2] == 13 && buffer[i - 1] == 10) {
+      buffer[i] = 0;
+      i = 0;
       dbg.print(buffer);
       return true;
     }
@@ -196,23 +190,23 @@ bool readTillEnd() {
   return false;
 }
 
-void updateLights(String inbound){
-  for(int x = 0; x < inbound.length(); x++){
-    int powerValue = inbound[x] - '0';
-    if (powerValue == 1){
-      ledOn(x);
+void updateLights(String inbound) {
+    for (int x = 0; x < inbound.length(); x++) {
+      int powerValue = inbound[x] - '0';
+      if (powerValue == 1) {
+        ledOn(x);
+      }
+      if (powerValue == 0) {
+        ledOff(x);
+      }
     }
-    if (powerValue == 0){
-      ledOff(x);
-    }
-  }
 }
 
 
-void ledOn(int number){
-  digitalWrite(number+2, HIGH);
+void ledOn(int number) {
+  digitalWrite(number + 2, HIGH);
 }
 
-void ledOff(int number){
-  digitalWrite(number+2, LOW);
+void ledOff(int number) {
+  digitalWrite(number + 2, LOW);
 }
